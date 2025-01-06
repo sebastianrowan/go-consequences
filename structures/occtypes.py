34ff460d2f499376753acf313c845542c96cd5d4
Sebastian Rowan <79868066+sebastianrowan@users.noreply.github.com>
2025-01-06 19:56:54 +0000

    occtypes.py and occtypes.json

---
+++ structures/occtypes.py
@@ -0,0 +1,173 @@
+# import numpy as np
+import pandas as pd
+# import geopandas as gpd
+import os
+import json
+from pprint import pprint
+
+
+def prettify_occtypes():
+    with open("occtypes.json", "r") as f:
+        occtypes = json.load(f)
+    
+    with open("occtypes_sbr.json", "w") as out:
+        json.dump(occtypes, out, indent=4)
+
+def build_null_df(df):
+    output = {
+        "damagefunctions": {
+            "depth": {
+                "source": "Rowan et al. (2024a)",
+                "damagedriver": "depth",
+                "damagefunction": {
+                    "xvalues":[0,0],
+                    "ydistributions": [
+                        {"type": "NormalDistribution","parameters":{"mean": 0,"sd": 0}},
+                        {"type": "NormalDistribution","parameters":{"mean": 0,"sd": 0}}
+                    ]
+                }
+            }
+        }
+    }
+
+    return(output)
+
+def build_damage_function(df):
+    output = {
+        "damagefunctions": {
+            "depth": {
+                "source": "Rowan et al. (2024a)",
+                "damagedriver": "depth",
+                "damagefunction": {
+                    "xvalues":[],
+                    "ydistributions": []
+                }
+            }
+        }
+    }
+    
+    for i, row in df.iterrows():
+        # $190/1000kg = $0.19/kg
+        mean = row['co2_cost_pct_mean']/0.190 # output will be kg CO2eq per total replacement value
+        sd = row['co2_cost_pct_sd']/0.190
+
+        ydist = {
+            "type": "NormalDistribution",
+            "parameters":{
+                "mean": mean,
+                "sd": sd
+            }
+        }
+
+        output['damagefunctions']['depth']['damagefunction']['xvalues'].append(row['flood_depth'])
+        output['damagefunctions']['depth']['damagefunction']['ydistributions'].append(ydist)
+
+    return(output)
+
+def build_mv_damage_function():
+    output = {
+        "damagefunctions": {
+            "depth": {
+                "source": "Rowan et al. (2024a)",
+                "damagedriver": "depth",
+                "damagefunction": {
+                    "xvalues":[1,2],
+                    "ydistributions": [
+                        {"type": "NormalDistribution","parameters":{"mean": 10,"sd": 0.1}},
+                        {"type": "NormalDistribution","parameters":{"mean": 20,"sd": 0.2}}
+                    ]
+                },
+                "damagevectormean": {
+                    "intercept": 11,
+                    "depth": 12,
+                    "sqft": 13,
+                    "n_bed": 14,
+                    "n_bath": 15,
+                    "n_car": 16,
+                    "depth_sqft": 17,
+                    "depth_n_bed": 18,
+                    "depth_n_bath": 19,
+                    "depth_n_car": 20
+                },
+                "damagevectorsd": {
+                    "intercept": 1,
+                    "depth": 1,
+                    "sqft": 1,
+                    "n_bed": 1,
+                    "n_bath": 1,
+                    "n_car": 1,
+                    "depth_sqft": 1,
+                    "depth_n_bed": 1,
+                    "depth_n_bath": 1,
+                    "depth_n_car": 1
+                }
+            }
+        }
+    }
+    return(output)
+
+def read_occtypes():
+    with open("occtypes.json", "r") as f:
+        occtypes = json.load(f)
+    
+    for ot in occtypes['occupancytypes'].keys():
+        print(ot)
+        # print(occtypes['occupancytypes'][ot]['componentdamagefunctions']['structure']['damagefunctions'].keys())
+        # print("----")
+    
+def print_dfs():
+    dfs = pd.read_parquet("rowan_2024a_dmg_fns.parquet")
+    dfs['co2_cost_pct_sd'] = (dfs['co2_cost_pct_mean'] - dfs['co2_cost_pct_low']) / 1.96
+    dfs = dfs[['occtype', 'flood_depth', 'co2_cost_pct_mean', 'co2_cost_pct_sd']]
+    dfs['flood_depth'] = dfs['flood_depth'].round(1)
+    dfs = dfs[(dfs['flood_depth'] % 1 == 0) & (dfs['flood_depth'] <= 16)]
+    print(dfs)
+
+
+def main():
+
+    dfs = pd.read_parquet("rowan_2024a_dmg_fns.parquet")
+    with open("occtypes_original.json", "r") as f:
+        occtypes = json.load(f)
+
+    dfs['co2_cost_pct_sd'] = (dfs['co2_cost_pct_mean'] - dfs['co2_cost_pct_low']) / 1.96
+    dfs = dfs[['occtype', 'flood_depth', 'co2_cost_pct_mean', 'co2_cost_pct_sd']]
+    dfs['flood_depth'] = dfs['flood_depth'].round(1)
+    dfs = dfs[(dfs['flood_depth']*10 % 1 == 0) & (dfs['flood_depth'] <= 16)]
+
+    dfs1 = dfs[dfs['occtype'] == "RES1-1S"]
+    dfs2 = dfs[dfs['occtype'] == "RES1-2S"]
+
+    df1 = build_damage_function(dfs1)
+    df2 = build_damage_function(dfs2)
+    dfnull = build_null_df(dfs1)
+
+    df_mv = build_mv_damage_function()
+    
+    occtypes_out = {"occupancytypes":{}}
+    for key, o in occtypes['occupancytypes'].items():
+        occtypes_out["occupancytypes"][key] = o
+        if(o['name'][0:7] == "RES1-1S"):
+            print(f"{o['name']} getting ghg df1")
+            occtypes_out['occupancytypes'][o['name']]['componentdamagefunctions']['greenhouse_gas'] = df1
+            occtypes_out['occupancytypes'][o['name']]['componentdamagefunctions']['greenhouse_gas2'] = df_mv
+        elif(o['name'][0:7] in ["RES1-2S", "RES1-3S", "RES1-SL", "RES3A", "RES3B"]):
+            print(f"{o['name']} getting ghg df2")
+            occtypes_out['occupancytypes'][o['name']]['componentdamagefunctions']['greenhouse_gas'] = df2
+            occtypes_out['occupancytypes'][o['name']]['componentdamagefunctions']['greenhouse_gas2'] = df_mv
+        else:
+            print(f"{o['name']} getting ghg dfnull")
+            occtypes_out['occupancytypes'][o['name']]['componentdamagefunctions']['greenhouse_gas'] = dfnull
+            occtypes_out['occupancytypes'][o['name']]['componentdamagefunctions']['greenhouse_gas2'] = df_mv
+
+    # print(occtypes_out)
+    # with open("occtypes_ghgrowan2024a.json", "w") as out:
+    with open("occtypes.json", "w") as out:
+        json.dump(occtypes_out, out, indent=4)
+
+
+
+
+if __name__ == "__main__":
+    os.chdir(os.path.dirname(os.path.realpath(__file__)))
+    main()
